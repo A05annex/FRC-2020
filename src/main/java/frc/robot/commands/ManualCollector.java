9b42cdc6badd041bf8932fddfffab685a815b93e
roy <rhall@rubota.com>
2020-02-06 21:09:22 -0800

    Progress on collector (arm and sweeper) programming.

--- src/main/java/frc/robot/commands/ManualCollector.java
+++ src/main/java/frc/robot/commands/ManualCollector.java
@@ -1,14 +1,9 @@
 package frc.robot.commands;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
-import frc.robot.subsystems.CollectorSubsystem;
+import frc.robot.subsystems.ArmSubsystem;
 
 // This is a really quick qnd dirty command to hook the sweeper motor to the driver joystic throttle, and the arm position
 // to the XBox controller right stick Y for testing. This is completely manual control so the builder/testers can
@@ -16,22 +11,19 @@ import frc.robot.subsystems.CollectorSubsystem;
 // buttons/sticks are they connected to.
 public class ManualCollector extends CommandBase {
 
-    private final Joystick m_stick;
     private final XboxController m_xbox;
-    private final CollectorSubsystem m_collectorSubsystem;
+    private final ArmSubsystem m_armSubsystem;
 
     /**
      *
-     * @param joyStick
      * @param xbox
-     * @param collectorSubsystem
+     * @param armSubsystem
      */
-    public ManualCollector(CollectorSubsystem collectorSubsystem, Joystick joyStick, XboxController xbox) {
-        m_stick = joyStick;
+    public ManualCollector(ArmSubsystem armSubsystem, XboxController xbox) {
         m_xbox = xbox;
-        m_collectorSubsystem = collectorSubsystem;
+        m_armSubsystem = armSubsystem;
         // Use addRequirements() here to declare subsystem dependencies.
-        addRequirements(collectorSubsystem);
+        addRequirements(armSubsystem);
     }
 
     // Called when the command is initially scheduled.
@@ -45,8 +37,7 @@ public class ManualCollector extends CommandBase {
         // What we are doing here is simple - we are setting the sweeper speed to the throttle of the joystick (the only
         // control that lets us easily set and hod a speed), and setting to position motor power based on the setting of the
         // xbox right stick Y
-        m_collectorSubsystem.setSweeperPower(m_stick.getThrottle());
-        m_collectorSubsystem.setPositionPower(m_xbox.getY(GenericHID.Hand.kRight));
+        m_armSubsystem.setPositionPower(m_xbox.getY(GenericHID.Hand.kRight));
     }
 
     // Called once the command ends or is interrupted.
