8250f38e8713e8715eaf99a7a02eb8f00f6804b7
Aden Cross <adenrudy23@gmail.com>
2020-02-26 16:21:07 -0800

    Added commands from auto branch

--- src/main/java/frc/robot/commands/AutoDrive.java
+++ src/main/java/frc/robot/commands/AutoDrive.java
@@ -50,7 +50,7 @@ public class AutoDrive extends CommandBase {
   @Override
   public void initialize() {
     startEncoder = m_driveSubsystem.getTotalPosition();
-    ramp = new RampInOut(0, m_distance, m_speed, .15, 30 * Constants.ROBOT.DRIVE_TICS_PER_INCH, .15, 40 * Constants.ROBOT.DRIVE_TICS_PER_INCH);
+    ramp = new RampInOut(0, m_distance, m_speed, .20, 20 * Constants.ROBOT.DRIVE_TICS_PER_INCH, .15, 30 * Constants.ROBOT.DRIVE_TICS_PER_INCH);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
--- src/main/java/frc/robot/commands/SetSweeperPower.java
+++ src/main/java/frc/robot/commands/SetSweeperPower.java
@@ -8,27 +8,32 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.SpinnerSubsystem;
 import frc.robot.subsystems.SweeperSubsystem;
 
 public class SetSweeperPower extends CommandBase {
 
   private final SweeperSubsystem m_sweeper;
+  private final SpinnerSubsystem m_spinner;
   private double m_power;
 
   /**
    * Creates a new SetSweeperPower.
    */
-  public SetSweeperPower(SweeperSubsystem sweeperSubsystem, double power) {
+  public SetSweeperPower(SweeperSubsystem sweeperSubsystem, SpinnerSubsystem spinnerSubsystem, double power) {
     m_sweeper = sweeperSubsystem;
+    m_spinner = spinnerSubsystem;
     m_power = power;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_sweeper);
+    addRequirements(m_spinner);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     m_sweeper.setSweeperPower(m_power);
+    m_spinner.setPower(-m_power);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -39,7 +44,6 @@ public class SetSweeperPower extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_sweeper.setSweeperPower(0);
   }
 
   // Returns true when the command should end.
