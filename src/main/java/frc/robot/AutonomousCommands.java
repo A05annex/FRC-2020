2b79cf2d45f728fbde8e3a7cc65a7b77c8126a1d
STHobbes <rhall@rubota.com>
2020-03-08 14:19:53 -0700

    Resolved merge issues

--- src/main/java/frc/robot/AutonomousCommands.java
+++ src/main/java/frc/robot/AutonomousCommands.java
@@ -4,10 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.commands.AutoDrive;
-import frc.robot.commands.AutoTurn;
-import frc.robot.commands.CollectorPidPosition;
-import frc.robot.commands.SetSweeperPower;
+import frc.robot.commands.*;
 
 
 /**
